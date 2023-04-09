6f30c39fd74455d37cad2285a5758a556f4ca5eb
Emilie <emilie.rich4rd@gmail.com>
2023-04-09 15:37:19 +0200

    Requirements + One product tests

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ response = requests.get(url)
 
 if response.ok:
     soup = BeautifulSoup(response.text, 'html.parser')
-    title = soup.find('article')
-    print(title)
+    title = soup.find('title')
+    tds = soup.findAll('td')
+    upc = tds[0]
+    priceAndTaxes = tds[2]
+    pricesNoTaxes = tds[3]
+    availability = tds[5]
 
+    print(title.text, upc.text, priceAndTaxes.text, pricesNoTaxes.text)
+
+"""
 with open('urls.csv', 'w') as file:
-    file.write(str(title))
+    file.write(str(title))*
+"""
 
