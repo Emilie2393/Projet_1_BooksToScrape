0803b30841808f45eb832d1f3f6180b9b8b2cab8
Emilie <emilie.rich4rd@gmail.com>
2023-04-16 18:41:14 +0200

    New def get_links and get_next

--- main.py
+++ main.py
@@ -1,23 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+url = "http://books.toscrape.com/catalogue/category/books/fiction_10/index.html"
 
 response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
 if response.ok:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    title = soup.find('title')
-    tds = soup.findAll('td')
-    upc = tds[0]
-    priceAndTaxes = tds[2]
-    pricesNoTaxes = tds[3]
-    availability = tds[5]
+    def get_links():
+        section = soup.findAll('div', {'class': 'image_container'})
+        for i in section:
+            for p in i.find_all('a'):
+                link = p['href']
+                print(link)
 
-    print(title.text, upc.text, priceAndTaxes.text, pricesNoTaxes.text)
+    get_links()
+
+    def get_next(pages_number):
+        if soup.find('li', {'class': 'next'}):
+            new_page = "page-"+pages_number+"html"
+            url.replace("index.html", new_page)
+            print(url)
+        else:
+            print("pas de next")
+
+    """ nombre de page """
+    pages = soup.find('li', {'class': 'current'}).text.strip()
+    print(pages[len(pages)-1])
+
+
+
+
+
+
+
+
 
 """
-with open('urls.csv', 'w') as file:
-    file.write(str(title))*
-"""
+if response.ok:
+    with open('test.csv', 'w') as test:
+        test.write('product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, '
+                   'number_available, product_description, category, review_rating, image_url\n')
 
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find('h1').text
+        tds = soup.findAll('td')
+        upc = tds[0].text
+        priceAndTaxes = tds[2].text
+        pricesNoTaxes = tds[3].text
+        availability = tds[5].text
+        description = soup.find('div', {'id': 'product_description'}).find_next('p').text
+        category = soup.findAll('a')[3].text
+        rating = soup.findAll('p')[2]['class'][1]
+        img = soup.find('img')['src']
+
+        test.write(url + '|' + upc + '|' + title + '|' + priceAndTaxes + '|' + pricesNoTaxes + '|' +
+                   availability + '|' + description + '|' + category + '|' + rating + '|' + img + '\n')
+                   """
