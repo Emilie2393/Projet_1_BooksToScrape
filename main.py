2571363aedd116a017d4d5fef2c6158dbcf0d1c0
Emilie <emilie.rich4rd@gmail.com>
2023-04-28 17:53:33 +0200

    update with pandas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pandas as pd
 
 home_url = "http://books.toscrape.com/index.html"
 
@@ -13,7 +13,7 @@ for i in category_urls:
     category_url = i['href']
     """ correct category's url """
     new_home_url = home_url.replace("/index.html", "")
-    full_link = (new_home_url+'/'+category_url)
+    full_link = (new_home_url + '/' + category_url)
     category_list.append(full_link)
 """ delete home page from category_list """
 category_list.pop(0)
@@ -35,6 +35,7 @@ for f in category_list:
                     link_href = p['href']
                     link = link_href.replace("../../..", "http://books.toscrape.com/catalogue")
                     links.append(link)
+
 
         def get_next(pages, next_url):
             new_page = "page-" + pages + ".html"
@@ -66,29 +67,64 @@ for f in category_list:
                     get_links(url)
         else:
             get_links(url)
-        print(links)
 
+        """get category name for csv name"""
+        category_name = soup.find('h1').text
 
-    """with open('test.csv', 'w', encoding="utf-8") as test:
-        test.write('product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available,'
-                   'product_description, category, review_rating, image_url\n')
+        """with open((category_name + '.csv'), 'w', encoding="utf-8") as data:
+            data.write('product_page_url' + ',' + 'upc' + ',' + 'title' + ',' + 'price_including_tax' + ','
+                       + 'price_excluding_tax' + ',' + 'number_available' + ',' + 'product_description' + ',' +
+                       'category' + ',' + 'review_rating' + ',' + 'image_url' + '\n')"""
+
+        products_page_url = []
+        upc = []
+        title = []
+        price_including_tax = []
+        price_excluding_tax = []
+        number_available = []
+        product_description = []
+        category = []
+        review_rating = []
+        image_url = []
 
         for j in links:
             url = j
+            products_page_url.append(url)
             response = requests.get(url)
             soup = BeautifulSoup(response.text, 'html.parser')
             if response.ok:
                 tds = soup.findAll('td')
-                upc = tds[0].text
-                priceAndTaxes = tds[2].text
-                pricesNoTaxes = tds[3].text
-                availability = tds[5].text
-                description = soup.find('div', {'id': 'product_description'}).find_next('p').text
-                description_bug = description.replace(';', ',')
-                category = soup.findAll('a')[3].text
-                rating = soup.findAll('p')[2]['class'][1]
-                img = soup.find('img')['src']
-                title = soup.find('h1').text
-                test.write(url + '|' + upc + '|' + title + '|' + priceAndTaxes + '|' + pricesNoTaxes + '|' +
-                           availability + '|' + description_bug + '|' + category + '|' + rating + '|' + img + '\n')"""
+                upc.append(tds[0].text)
+                price_including_tax.append(tds[2].text[2:7])
+                price_excluding_tax.append(tds[3].text[2:7])
+                number_available.append(tds[5].text)
+                description = soup.find('div', {'id': 'product_description'})
+                if description is None:
+                    product_description.append("")
+                else:
+                    product_description.append(description.find_next('p').text)
+                    """product_description.append(description.replace(',', ' '))"""
+                category.append(category_name)
+                review_rating.append(soup.findAll('p')[2]['class'][1])
+                image_url.append(soup.find('img')['src'])
+                title.append(soup.find('h1').text)
 
+        datas = {
+            'products_page_url': products_page_url,
+            'upc': upc,
+            'title': title,
+            'price_excluding_tax': price_excluding_tax,
+            'price_including_tax': price_including_tax,
+            'number_available': number_available,
+            'product_description': product_description,
+            'category': category,
+            'review_rating': review_rating,
+            'image_url': image_url
+        }
+
+        df = pd.DataFrame(datas)
+    df.to_csv((category_name + '.csv'), index=False, sep=',', encoding='utf-8')
+    df
+
+    print(title)
+    print(product_description)
