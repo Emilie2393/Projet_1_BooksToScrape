16e9805854a35d25cc8cb55d8b0433103f02efbd
Emilie <emilie.rich4rd@gmail.com>
2023-06-08 10:30:30 +0200

    threads version

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 import re
+import threading
 
 """get html source"""
 
@@ -18,31 +19,11 @@ def get_html(from_url):
         print("HTTP Error", err)
 
 
-"""get one book"""
-
-
-def get_book(html_soup):
-    products_page_url.append(url)
-    tds = html_soup.findAll('td')
-    upc.append(tds[0].text)
-    prices_including_tax.append(tds[2].text)
-    prices_excluding_tax.append(tds[3].text)
-    number_available.append(tds[5].text)
-    description = html_soup.find('div', {'id': 'product_description'})
-    if description is None:
-        products_description.append("")
-    else:
-        products_description.append(description.find_next('p').text)
-    category.append(category_name)
-    reviews_rating.append(html_soup.findAll('p')[2]['class'][1])
-    images_url.append((html_soup.find('img')['src']).replace('../..', 'http://books.toscrape.com'))
-    titles.append(html_soup.find('h1').text)
-
-
 """complete books_list with books of one page"""
 
 
 def get_links(next_url):
+    books_list = []
     next_soup = get_html(next_url)
     section = next_soup.findAll('div', {'class': 'image_container'})
     for books in section:
@@ -51,6 +32,7 @@ def get_links(next_url):
             link_href = a['href']
             link = link_href.replace("../../..", "http://books.toscrape.com/catalogue")
             books_list.append(link)
+    return books_list
 
 
 """get next page of a category"""
@@ -64,7 +46,6 @@ def get_next(pages, next_url):
         old_page = int(pages) - 1
         text_page = str(old_page)
         new_url = next_url.replace(("page-" + text_page + ".html"), new_page)
-    global url
     url = new_url
     return url
 
@@ -93,32 +74,12 @@ for i in category_urls:
 """ delete home page from category_list """
 category_list.pop(0)
 
-for f in category_list:
+first_half_list = int((len(category_list)) / 2)
 
-    url = f
+
+def final_scraping(book):
+    url = book
     soup = get_html(url)
-    books_list = []
-
-    """pages number or None"""
-    pages_check = soup.find('li', {'class': 'current'})
-
-    """get book's links of each pages"""
-    if pages_check is not None:
-        pages_strip = pages_check.text.strip()
-        pages_number = pages_strip[len(pages_strip) - 1]
-        for page in range(1, (int(pages_number) + 1)):
-            """ get index.html links only """
-            if page == 1:
-                get_links(url)
-            else:
-                """get next url page and get their product's links"""
-                get_next(str(page), url)
-                get_links(url)
-    else:
-        get_links(url)
-
-    """get category name for csv file"""
-    category_name = soup.find('h1').text
 
     products_page_url = []
     upc = []
@@ -131,10 +92,56 @@ for f in category_list:
     reviews_rating = []
     images_url = []
 
+    links1 = []
+    links2 = []
+    links3 = []
+
+    """pages number or None"""
+    pages_check = soup.find('li', {'class': 'current'})
+
+    """get book's links of each pages"""
+    if pages_check is not None:
+        pages_strip = pages_check.text.strip()
+        pages_number = pages_strip[len(pages_strip) - 1]
+        for page in range(1, (int(pages_number) + 1)):
+            """ get index.html links only """
+            if page == 1:
+                links1 = get_links(url)
+            else:
+                """get next url page and get their product's links"""
+                links2 = get_links(get_next(str(page), url))
+    else:
+        links3 = get_links(url)
+
+    books_list = links1 + links2 + links3
+
+    """get category name for csv file"""
+    category_name = soup.find('h1').text
+
     for j in books_list:
-        url = j
-        soup = get_html(url)
-        get_book(soup)
+        book_url = j
+        book_soup = get_html(book_url)
+
+        """get one book"""
+
+        def get_book(html_soup):
+            products_page_url.append(book_url)
+            tds = html_soup.findAll('td')
+            upc.append(tds[0].text)
+            prices_including_tax.append(tds[2].text)
+            prices_excluding_tax.append(tds[3].text)
+            number_available.append(tds[5].text)
+            description = html_soup.find('div', {'id': 'product_description'})
+            if description is None:
+                products_description.append("")
+            else:
+                products_description.append(description.find_next('p').text)
+            category.append(category_name)
+            reviews_rating.append(html_soup.findAll('p')[2]['class'][1])
+            images_url.append((html_soup.find('img')['src']).replace('../..', 'http://books.toscrape.com'))
+            titles.append(html_soup.find('h1').text)
+
+        get_book(book_soup)
 
     datas = {
         'products_page_url': products_page_url,
@@ -155,15 +162,27 @@ for f in category_list:
 
     """images file creation with os"""
     os.mkdir(category_name)
-
     """titles modification"""
     for title in range(len(titles)):
         img_title = (re.sub("[':,;!#*?/.-]", '', titles[title])).replace(" ", "_").replace('"', '')
         titles[title] = img_title
-
     """path attribution for each images with urllib.request"""
     for img in range(len(images_url)):
         path = category_name + "/" + titles[img] + ".jpg"
         urllib.request.urlretrieve(images_url[img], path)
 
 
+def first_half():
+    for i in range(first_half_list):
+        final_scraping(category_list[i])
+
+
+def second_half():
+    for e in range((first_half_list + 1), len(category_list)):
+        final_scraping(category_list[e])
+
+
+thread1 = threading.Thread(target=first_half)
+thread2 = threading.Thread(target=second_half)
+thread1.start()
+thread2.start()
